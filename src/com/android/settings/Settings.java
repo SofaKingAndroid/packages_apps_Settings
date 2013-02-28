2875fc652d6fe6258c681a0631c787f0cd80d138
Koushik Dutta <koushd@gmail.com>
2013-02-28 14:49:24 -0800

    tweaked settings
    Change-Id: I06d320e91b2fe39d1f84af1f161a43498be32c4a

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -876,7 +876,6 @@ public class Settings extends PreferenceActivity
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
-    public static class AnonymousStatsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
     public static class NotificationStationActivity extends Settings { /* empty */ }
